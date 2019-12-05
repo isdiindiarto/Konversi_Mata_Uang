a10968cd40e373760be10553a5a480d6b019c6f9
SdInd <indisdi76@gmail.com>
2019-12-05 11:46:32 +0700

    Pertemuan 9

---
+++ app/src/main/java/com/example/konversi/konv_pound.kt
@@ -0,0 +1,12 @@
+package com.example.konversi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class konv_pound : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_konv_pound)
+    }
+}
